41f7b629f917f8d63e9fe8e41fe617552a5661e9
Hana Wael Soliman <solimanhana2007@gmail.com>
2026-07-26 16:37:57 +0300

    Initial commit

---
+++ import numpy as np.py
@@ -0,0 +1,21 @@
+import numpy as np
+A =   np.array([
+                      [16, 26],
+                      [6, 8]
+                     ])
+B = np.array([
+                      [10, 2],
+                      [39, 4]
+                     ])
+print("Matrix A :")
+print(A)
+print("\nMatrix B :")
+print(B)
+print("\nThe sum  of A + B is:")
+print(A + B)
+print("\nThe difference of A - B is:" )
+print(A - B)
+print("\nThe product of A * B is:")
+print(A * B)
+print("\nThe quotient of A / B is:")
+print(A / B)
